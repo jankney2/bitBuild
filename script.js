0eeb159d872878caeed49ffe76e63c699e6d1116
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-10-23 09:43:25 -0600

    morning commit

--- script.js
+++ script.js
@@ -4,6 +4,23 @@
 
 //nav comes back in about 1/4 down the page
 
+let nav= document.getElementsByTagName('nav')[0]
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY<400){
+        nav.classList.remove('scrollNav')
+        nav.classList.remove('scrolling')
+        
+        
+    }
+    if(window.scrollY>100){
+        nav.classList.add('scrolling')
+        
+    }
+    if(window.scrollY>500) {
+        nav.classList.add('scrollNav')
+    }  
+})
 
 //get started today form
 
